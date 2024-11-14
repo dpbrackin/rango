b5232ba826e10be856c860d07603f297cc6805b6
dpbrackin <dp.brackin@gmail.com>
2024-11-15 02:58:39 +0800

    feat: event bus

---
+++ lib/go/platform/event_bus/event_bus.go
@@ -0,0 +1,55 @@
+package eventbus
+
+import "sync"
+
+type Event struct {
+	Topic string
+	Data  interface{}
+}
+
+type eventChannel chan Event
+
+type channelSlice []eventChannel
+
+type EventBus struct {
+	subscribers map[string]channelSlice
+	mu          sync.RWMutex
+}
+
+func New() *EventBus {
+	return &EventBus{
+		subscribers: make(map[string]channelSlice),
+	}
+}
+
+func (e *EventBus) Subscribe(topic string) <-chan Event {
+	ch := make(chan Event)
+
+	e.mu.Lock()
+	e.subscribers[topic] = append(e.subscribers[topic], ch)
+	e.mu.Unlock()
+
+	return ch
+}
+
+func (e *EventBus) Publish(event Event) {
+	e.mu.RLock()
+	defer e.mu.RUnlock()
+
+	for _, ch := range e.subscribers[event.Topic] {
+		go func(c chan Event) {
+			c <- event
+		}(ch)
+	}
+}
+
+func (e *EventBus) Close() {
+	e.mu.Lock()
+	e.mu.Unlock()
+
+	for _, subscribers := range e.subscribers {
+		for _, ch := range subscribers {
+			close(ch)
+		}
+	}
+}
---
+++ lib/go/platform/event_bus/event_bus_test.go
@@ -0,0 +1,26 @@
+package eventbus_test
+
+import (
+	eventbus "rango/platform/event_bus"
+	"reflect"
+	"testing"
+)
+
+func TestEventBus(t *testing.T) {
+	eventBus := eventbus.New()
+	testTopic := "topic.test"
+	testData := "test data"
+
+	wantedEvent := eventbus.Event{
+		Topic: testTopic,
+		Data:  testData,
+	}
+
+	subscriber := eventBus.Subscribe(testTopic)
+
+	eventBus.Publish(wantedEvent)
+
+	if actualEvent := <-subscriber; !reflect.DeepEqual(wantedEvent, actualEvent) {
+		t.Errorf("Did not get the published event. Wanted: %v. Got: %v", wantedEvent, actualEvent)
+	}
+}
